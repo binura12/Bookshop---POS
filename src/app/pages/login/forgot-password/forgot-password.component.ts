d62d4c6dde5f54bda22570b8380f4cc3f8a1cbc8
B1NUR4 <binuraofficial32@gmail.com>
2024-11-23 02:44:04 +0530

    reset password done and fix some bugs

--- src/app/pages/login/forgot-password/forgot-password.component.ts
+++ src/app/pages/login/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
@@ -11,10 +12,18 @@ import { RouterLink } from '@angular/router';
   styleUrl: './forgot-password.component.css'
 })
 export class ForgotPasswordComponent {
-  isEmailLblVisible:boolean = false;
+  enteredEmail: string = "";
+  isEmailLblVisible: boolean = false;
   otp: string[] = ['', '', '', '', '', ''];
   isOTPCorrect: boolean = false;
   feedbackMessage: string = '';
+  otpCode: string = '';
+  isEmailVerified: boolean = false;
+  newPassword: string = '';
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   onOTPInput(event: Event, index: number): void {
     const inputElement = event.target as HTMLInputElement;
@@ -31,10 +40,57 @@ export class ForgotPasswordComponent {
     // Check if the OTP is complete and validate it
     if (this.otp.every((digit) => digit !== '')) {
       const enteredOTP = this.otp.join('');
-      const expectedOTP = '123456'; // Replace with your expected OTP
+      const expectedOTP = this.otpCode;
 
       this.isOTPCorrect = enteredOTP === expectedOTP;
       this.feedbackMessage = this.isOTPCorrect ? 'Correct OTP!' : 'Incorrect OTP. Please try again.';
+      if (this.isOTPCorrect) {
+        this.isEmailVerified = true;
+      }
     }
+  }
+
+  resetPassword(){
+    if (this.isPasswordStrong(this.newPassword)) {
+      this.http.put<boolean>(`http://localhost:8080/admin/update-password`, null, {
+        params: {
+          email: this.enteredEmail,
+          newPassword: this.newPassword
+        }
+      }).subscribe({
+        next: (data) => {
+          if (data) {
+            alert('Password updated successfully');
+          } else {
+            alert('Failed to update password');
+          }
+        }
+      });
+    } else {
+      alert('Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.');
+    }
+  }
+
+  verifyEmail() {
+    this.isEmailLblVisible = false;
+    this.http.get<any>(`http://localhost:8080/admin/check-by-email/${this.enteredEmail}`).subscribe({
+      next: (data) => {
+        if (data.exists) {
+          this.otpCode = data.otp;
+        } else {
+          this.isEmailLblVisible = true;
+          this.enteredEmail = '';
+        }
+      }
+    });
+  }
+
+  private isPasswordStrong(password: string): boolean {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChars = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    return password.length >= minLength && hasLowerCase && hasUpperCase && hasNumbers && hasSpecialChars;
   }
 }
